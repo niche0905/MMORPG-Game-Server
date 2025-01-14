fb199d39112d0c6dcc67d91c6f6b67820f87b238
niche0905 <yudle537@gmail.com>
2025-01-14 22:57:29 +0900

    Game 클래스 초안
    Game 클래스의 초안을 작성하였다
    추가로 필요해 보이는 것은 추석으로 작성하였다

---
+++ Client/Game.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include "Game.h"
+
+
+// 게임 실행 루프 (Input 받고, timer 업데이트, Scene Update, Draw)
+void Game::Run()
+{
+
+}
+
+// 게임 요소 업데이트 (Scene 업데이트)
+void Game::Update(int64 delta_time)
+{
+
+}
+
+// 게임 요소 렌더링 (Scene Draw)
+void Game::Draw()
+{
+
+}
+
+// Input 받기 (받아서 Scene으로 넘기기)
+void Game::HandleInput()
+{
+
+}
---
+++ Client/Game.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+class Game
+{
+private:
+	sf::RenderWindow window;
+
+	// TODO : Scene을 가지고 있고 해당 Scene을 그려주고 돌려주어야 함
+
+private:
+	void Run();
+	void Update(int64 delta_time);
+	void Draw();
+	void HandleInput();
+
+	// TODO : SceneLoad 함수를 만들어서 씬을 로드 해야 함
+
+};
+
