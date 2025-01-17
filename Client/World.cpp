5ed025e925ede2c6d6f00c26d5b3261ebcd4d562
niche0905 <yudle537@gmail.com>
2025-01-18 01:44:40 +0900

    World 메서드 주석 작성
    World 메서드의 까먹었던 주석을 달아주었다

--- Client/World.cpp
+++ Client/World.cpp
@@ -2,21 +2,25 @@
 #include "World.h"
 
 
+// 월드 기본 생성자
 World::World()
 {
 
 }
 
+// 월드 그리기 함수
 void World::Draw(sf::RenderWindow& window)
 {
 
 }
 
+// 월드 파일에서 불러오기
 void World::LoadWorld()
 {
 
 }
 
+// 월드 파일에 저장하기
 void World::StoreWorld()
 {
 
