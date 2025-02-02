a282d9f69db2605d3258a438a69d7b7e5cb4eef6
niche0905 <yudle537@gmail.com>
2025-02-02 15:50:41 +0900

    RuntimeLibrary 멀티스레드 문제 + 링커 추가 종속성 문제 해결
    RuntimeLibrary 멀티스레드 불일치 문제로 프로젝트 속성에서 설정을 통해 하나로 통일하도록 명시해 주었으며
    링커의 추가 종속성을 추가하여 링커 문제를 해결할 수 있었다

--- Client/Game.cpp
+++ Client/Game.cpp
@@ -8,7 +8,7 @@ Game::Game()
 	: window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "MMORPG GAME")
 {
 	// TODO : 아래의 코드가 BaseScene이 아니라 BaseScene을 상속받는 Scene이어야 함
-	scene = std::make_shared<BaseScene>();
+	scene = std::make_shared<GameScene>();
 
 	// TODO : Loading이 끝나고 나서 last_time을 초기화 해야 함
 	last_time = Epoch::now();
