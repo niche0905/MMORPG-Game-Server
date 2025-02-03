5a4de976a578b191f16fd317991054787bc230ff
niche0905 <yudle537@gmail.com>
2025-02-03 14:03:30 +0900

    BaseScene 수정
    BaseScene에 GetCameraCenter 메서드가 필요할 것 같아 추가하였다
    Game에서 window를 가지고 view를 설정할 수 있기에
    아니면 그냥 Scene 자체에서 window를 받아서 처리하는것도 가능할수도?

--- Client/BaseScene.h
+++ Client/BaseScene.h
@@ -17,6 +17,7 @@ public:
 	virtual void Update(const int64 delta_time) = 0;
 	virtual void Draw(sf::RenderWindow& window) = 0;
 	virtual void HandleInput(const sf::Event& event) = 0;
+	virtual sf::Vector2f GetCameraCenter() const = 0;
 
 protected:	// 게임에 필요한 함수들
 private:
