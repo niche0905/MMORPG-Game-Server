42b6b770dc1cc1cea6c0985b5c5153859064aa13
Niche <73772820+niche0905@users.noreply.github.com>
2025-02-21 15:01:50 +0900

    Animation 생성자 수정
    Animation에 Texture에서 시작하는 위치인 offset이 필요하다 판단하여 생성자에서 값을 받아서 내부적으로 가지고 있을 수 있게 변경하였다

--- Client/Animation.cpp
+++ Client/Animation.cpp
@@ -2,8 +2,10 @@
 #include "Animation.h"
 
 
-Animation::Animation(sf::Sprite& sprite, int frame_w, int frame_h, int num_frames, float frame_time)
-	: frame_width{ frame_w }
+Animation::Animation(sf::Sprite& sprite, int start_x, int start_y, int frame_w, int frame_h, int num_frames, float frame_time)
+	: offset_x{ start_x }
+	, offset_y{ start_y }
+	, frame_width{ frame_w }
 	, frame_height{ frame_h }
 	, number_of_frames{ num_frames }
 	, frame_flip_time{ frame_time }
--- Client/Animation.h
+++ Client/Animation.h
@@ -4,6 +4,7 @@
 class Animation
 {
 private:
+	int offset_x, offset_y;
 	int frame_width, frame_height;
 	int number_of_frames;
 	float frame_flip_time;
@@ -12,7 +13,7 @@ private:
 	float elapsed_time;
 
 public:
-	Animation(sf::Sprite& sprite, int frame_w, int frame_h, int num_frames, float frame_time);
+	Animation(sf::Sprite& sprite, int start_x, int start_y, int frame_w, int frame_h, int num_frames, float frame_time);
 
 	// TODO : 애니메이션에 필요한 함수들 만들기
 	// TODO : 구조를 어떻게 가져갈지 생각하기
