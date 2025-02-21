536cace87f4a6a509ddebfbe2f93805e63b31944
Niche <73772820+niche0905@users.noreply.github.com>
2025-02-22 00:53:34 +0900

    PresentAnimation 초안 작성
    AnimationObject에 멤버 변수로 존재할 class PresentAnimation의 초안을 작성하였다
    조금 더 구조적인 고민이 필요해 보인다

---
+++ Client/PresentAnimation.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+class PresentAnimation
+{
+private:
+	std::string animation_name;
+	int current_frame;
+	int64 elapsed_time;
+
+	// TODO : 추가로 생각해 볼 것
+	//		  애니메이션 루프 true / false 설정, 이후 돌아가기 위한 stack?
+
+};
+
