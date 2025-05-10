d923ed43e1e9043c7eeee11d9fbfc92bdac0fc77
niche0905 <yudle537@gmail.com>
2025-05-10 23:15:31 +0900

    상수 정의
    Core 라이브러리에 상수를 정의 하였다

---
+++ Core/Defines.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+constexpr short PORT_NUM = 8252;
+constexpr int BUF_SIZE = 1024;
+
+constexpr int MAX_NAME_LEN = 20;
+
+constexpr unsigned short MAX_WIDTH = 400;
+constexpr unsigned short MAX_HEIGHT = 400;
