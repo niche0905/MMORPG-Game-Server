d35190bf6a0bde9287836244b0a2b1670f03f41d
niche0905 <yudle537@gmail.com>
2025-06-05 21:09:43 +0900

    DatabaseEvent 추가
    DatabaseEvent라는 구조체를 추가하였다

---
+++ Server/DatabaseEvent.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "DatabaseEvent.h"
+
+
+DatabaseEvent::DatabaseEvent()
+	: DatabaseEvent{ -1, -1 }
+{
+
+}
+
+DatabaseEvent::DatabaseEvent(int64 id, uint8 sp_id)
+	: _id{ id }
+	, _sp_id{ sp_id }
+{
+
+}
---
+++ Server/DatabaseEvent.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+struct DatabaseEvent
+{
+	int64	_id;
+	uint8	_sp_id;	// stored procedure ID
+	// TODO: 추가 정보가 필요하다면 추가하기 (권장되지 않음) clients에서 꺼내 쓰자
+
+	DatabaseEvent();
+	DatabaseEvent(int64 id, uint8 sp_id);
+};
+
