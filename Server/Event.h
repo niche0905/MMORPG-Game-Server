9c8d771325e3cdca010634cb77d27c22bcdbc96b
niche0905 <yudle537@gmail.com>
2025-06-09 22:38:20 +0900

    Event 구조체 생성
    Timer Event에 사용할 Event 구조체를 구성하였다

---
+++ Server/Event.h
@@ -0,0 +1,32 @@
+#pragma once
+
+
+struct Event
+{
+private:
+	using time_point = std::chrono::system_clock::time_point;
+
+public:
+	enum EventType
+	{
+		EVENT_START = 0,
+
+		EV_RANDOM_MOVE,
+
+		EVENT_END
+	};
+
+public:
+	uint64			_id;
+	time_point		_reserved_time;
+	EventType		_event_type;
+	// TODO: 필요한 정보 있으면 추가하기
+
+public:
+	constexpr bool operator<(const Event& ev) const
+	{
+		return (_reserved_time > ev._reserved_time);
+	}
+	
+};
+
