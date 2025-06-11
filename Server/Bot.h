00056cae1b33edd634fdad9b1959b839d0ce259e
niche0905 <yudle537@gmail.com>
2025-06-11 22:35:44 +0900

    Bot 기본 필드 변경
    상속을 상정해 두고 Bot의 기본 필드를 추가하였다
    필요 시 더 추가해야 한다

--- Server/Bot.h
+++ Server/Bot.h
@@ -4,10 +4,17 @@
 
 class Bot : public Creature
 {
-private:
+protected:
 	std::atomic_bool		_is_active = false;		// 주위에 플레이어가 있어서 활동 중인가?
 
 	FSM						_fsm;
+
+	bool					_is_invincibility;		// 무적인지 (상호 공격 불가 몬스터)
+	bool					_is_neutrality;			// 중립 몹인지 (선공을 하지 않고 피격 시)
+	bool					_is_peace;				// 공격을 하지 않는 몹인지
+	bool					_is_fix;				// 고정 몹인지 (가만히 만 있는지)
+
+	// TODO: 필요 시 더 추가할 것
 
 public:
 	Bot();
