70d1f15bcde7a12bfc10065dbb3a49183f3bbff9
niche0905 <yudle537@gmail.com>
2025-06-11 13:57:46 +0900

    Packet 변경
    Packet을 1차로 수정해 보았다
    필요한 부분 추가 및 더 효율적일 수 있게 수정을 하였다

--- Core/Defines.h
+++ Core/Defines.h
@@ -10,36 +10,11 @@ constexpr int32 NUM_MONSTER = 200000;
 
 constexpr int32 CACHE_LINE_SIZE = 64;
 
-constexpr int8 MAX_NAME_LEN = 20;
-constexpr int16 MAX_CHAT_LENGTH = 245;
-// size 1byte를 고수하기 위해... 255 -> 245로 수정
-
 constexpr uint16 MAX_WIDTH = 2000;
 constexpr uint16 MAX_HEIGHT = 2000;
 
 constexpr uint64 INVALID_ID = UINT64_MAX;
 
-
-enum PacketID : uint8
-{
-	PACKET_START,
-
-	S2C_LOGIN_ALLOW = 1,
-	S2C_LOGIN_FAIL,
-	S2C_MOVE,
-	S2C_ENTER,
-	S2C_LEAVE,
-	S2C_CHAT,
-	S2C_STAT_CHANGE,
-
-	C2S_LOGIN,
-	C2S_MOVE,
-	C2S_CHAT,
-	C2S_ATTACK,
-	C2S_TELEPORT,
-
-	PACKET_END
-};
 
 enum MOVE : uint8
 {
--- Core/Protocol.h
+++ Core/Protocol.h
@@ -3,6 +3,44 @@
 #include "Defines.h"
 
 
+constexpr int8 MAX_NAME_LEN = 20;
+constexpr int16 MAX_CHAT_LENGTH = 245;
+// size 1byte를 고수하기 위해... 255 -> 245로 수정
+
+enum PacketID : uint8
+{
+	PACKET_START,
+
+//---------------------------------------------------------------------------//
+
+	S2C_LOGIN_ALLOW = 1,		// 스스로 보내는 로그인 허락 및 정보
+	S2C_LOGIN_FAIL,				// 스스로 보내는 로그린 실패와 이유
+	S2C_MOVE_SELF,				// 스스로 위치 Update (스트레스 테스트용)
+	S2C_STAT_CHANGE,			// 스스로 보내는 스텟 변경
+	S2C_DAMAGE,					// 스스로 공격에 대한 Damage 출력용
+	S2C_LEVEL_UP,				// 스스로 레벨이 올랐음을 알려주는 패킷
+	S2C_EXP_UP,					// 스스로 경험치가 올랐음을 알려주는 패킷
+
+	S2C_MOVE,					// 누군가 움직였다
+	S2C_ENTER,					// 누군가 들어왔다
+	S2C_LEAVE,					// 누군가 떠났다
+	S2C_CHAT,					// 누군가 채팅했다
+	S2C_ATTACK,					// 누군가 공격했다
+	S2C_HP_CHANGE,				// 누군가 체력이 바뀌었다
+	S2C_LEVEL_CHANGE,			// 누군가 레벨이 바뀌었다
+
+
+	C2S_LOGIN,					// 로그인 요청
+	C2S_MOVE,					// 이동 요청
+	C2S_TELEPORT,				// TP 요청
+	C2S_CHAT,					// 채팅 송신
+	C2S_ATTACK,					// 공격
+
+//---------------------------------------------------------------------------//
+
+	PACKET_END
+};
+
 #pragma pack(push, 1)
 
 struct BASE_PACKET
@@ -23,15 +61,17 @@ struct SC_LOGIN_ALLOW_PACKET	: public BASE_PACKET
 	int16	_x, _y;
 	uint16	_max_hp;
 	uint16	_hp;
-	uint8	_level;
-	uint32	_exp;
+	uint8	_visual_info;
+	uint32	_level;
+	uint64	_exp;
 
-	SC_LOGIN_ALLOW_PACKET(uint64 id, int16 x, int16 y, uint16 max_hp, uint16 hp, uint8 level, uint32 exp)
+	SC_LOGIN_ALLOW_PACKET(uint64 id, int16 x, int16 y, uint16 max_hp, uint16 hp, uint8 visual_info, uint32 level, uint64 exp)
 		: BASE_PACKET{ sizeof(SC_LOGIN_ALLOW_PACKET), S2C_LOGIN_ALLOW }
 		, _id{ id }
 		, _x{ x }, _y{ y }
 		, _max_hp{ max_hp }
 		, _hp{ hp }
+		, _visual_info{ visual_info }
 		, _level{ level }
 		, _exp{ exp } { }
 };
@@ -53,17 +93,67 @@ struct SC_LOGIN_FAIL_PACKET		: public BASE_PACKET
 		, _reason{ reason } { }
 };
 
+struct SC_MOVE_SELF_PACKET : public BASE_PACKET
+{
+	int16	_x, _y;
+	uint64	_move_time;
+
+	SC_MOVE_SELF_PACKET(int16 x, int16 y, uint64 move_time)
+		: BASE_PACKET{ sizeof(SC_MOVE_SELF_PACKET), S2C_MOVE_SELF }
+		, _x{ x }, _y{ y }
+		, _move_time{ move_time } { }
+};
+
+struct SC_STAT_CHANGE_PACKET : public BASE_PACKET
+{
+	uint16	_max_hp;
+	uint16	_hp;
+
+	SC_STAT_CHANGE_PACKET(uint16 max_hp, uint16 hp)
+		: BASE_PACKET{ sizeof(SC_STAT_CHANGE_PACKET), S2C_STAT_CHANGE }
+		, _max_hp{ max_hp }
+		, _hp{ hp } { }
+};
+
+struct SC_DAMAGE_PACKET : public BASE_PACKET
+{
+	// TODO
+
+	SC_DAMAGE_PACKET()
+		: BASE_PACKET{ sizeof(SC_DAMAGE_PACKET), S2C_DAMAGE }
+		 { }
+
+};
+
+struct SC_LEVEL_UP_PACKET : public BASE_PACKET
+{
+	uint32	_level;
+	uint64	_exp;
+
+	SC_LEVEL_UP_PACKET(uint32 level, uint64 exp)
+		: BASE_PACKET{ sizeof(SC_LEVEL_UP_PACKET), S2C_LEVEL_UP }
+		, _level{ level }
+		, _exp{ exp } { }
+};
+
+struct SC_EXP_UP_PACKET : public BASE_PACKET
+{
+	uint64	_exp;
+
+	SC_EXP_UP_PACKET(uint64 exp)
+		: BASE_PACKET{ sizeof(SC_EXP_UP_PACKET), S2C_EXP_UP }
+		, _exp{ exp } { }
+};
+
 struct SC_MOVE_PACKET			: public BASE_PACKET
 {
 	uint64	_id;
 	int16	_x, _y;
-	uint64	_move_time;
 
-	SC_MOVE_PACKET(uint64 id, int16 x, int16 y, uint64 move_time)
+	SC_MOVE_PACKET(uint64 id, int16 x, int16 y)
 		: BASE_PACKET{ sizeof(SC_MOVE_PACKET), S2C_MOVE }
 		, _id{ id }
-		, _x{ x }, _y{ y }
-		, _move_time{ move_time } { }
+		, _x{ x }, _y{ y } { }
 };
 
 struct SC_ENTER_PACKET			: public BASE_PACKET
@@ -101,20 +191,37 @@ struct SC_CHAT_PACKET			: public BASE_PACKET
 		, _id{ id } { strcpy_s(_message, message); }
 };
 
-struct SC_STAT_CHANGE_PACKET	: public BASE_PACKET
+struct SC_ATTACK_PACKET : public BASE_PACKET
 {
 	uint64	_id;
-	int16	_hp;
-	uint8	_level;
-	uint32	_exp;
+	int8	_atk_type;
 
-	SC_STAT_CHANGE_PACKET(uint64 id, int16 hp, uint8 level, uint32 exp)
-		: BASE_PACKET{ sizeof(SC_STAT_CHANGE_PACKET), S2C_STAT_CHANGE }
+	SC_ATTACK_PACKET(uint64 id, int8 atk_type)
+		: BASE_PACKET{ sizeof(SC_ATTACK_PACKET), S2C_ATTACK }
 		, _id{ id }
-		, _hp{ hp }
-		, _level{ level }
-		, _exp{ exp } { }
+		, _atk_type{ atk_type } { }
+};
 
+struct SC_HP_CHANGE_PACKET : public BASE_PACKET
+{
+	uint64	_id;
+	uint16	_hp;
+
+	SC_HP_CHANGE_PACKET(uint64 id, uint16 hp)
+		: BASE_PACKET{ sizeof(SC_HP_CHANGE_PACKET), S2C_HP_CHANGE }
+		, _id{ id }
+		, _hp{ hp } { }
+};
+
+struct SC_LEVEL_CHANGE_PACKET : public BASE_PACKET
+{
+	uint64	_id;
+	uint32	_level;
+
+	SC_LEVEL_CHANGE_PACKET(uint64 id, uint32 level)
+		: BASE_PACKET{ sizeof(SC_LEVEL_CHANGE_PACKET), S2C_LEVEL_CHANGE }
+		, _id{ id }
+		, _level{ level } { }
 };
 
 struct CS_LOGIN_PACKET			: public BASE_PACKET
@@ -146,7 +253,6 @@ struct CS_CHAT_PACKET			: public BASE_PACKET
 		{ strcpy_s(_message, message); }
 };
 
-// 해당 공격을 다른 클라이언트에도 알려야 하지 않을까? (비주얼을 동기화 하기 위해)
 struct CS_ATTACK_PACKET			: public BASE_PACKET
 {
 	int8	_atk_type;
