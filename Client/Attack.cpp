3999718c153bd31af1d371bd12101f92b22abb28
niche0905 <yudle537@gmail.com>
2025-06-13 03:49:02 +0900

    Attack 수정
    Attack을 필요에 의해 수정했다

--- Client/Attack.cpp
+++ Client/Attack.cpp
@@ -3,22 +3,30 @@
 #include "TextureManager.h"
 
 Attack::Attack()
-	: Attack{ "", {0,0}, 2000000 }
+	: Attack{ "standard_atk" }
 {
 
 }
 
-Attack::Attack(std::string text, sf::Vector2i pos, int64 t_point)
-	: _remove_time{ t_point }
+Attack::Attack(std::string text)
 {
+	_attack_obj.Hide();
+
 	TextureManager::Instance().LoadTexture("standard_atk", "./Resource/Texture/standard_atk.png");
-	_attack_obj.SetSprite("standard_atk");
-	_attack_obj.ForceMove(pos);
+	_attack_obj.SetSprite(text);
 }
 
 bool Attack::operator<(const Attack& other) const
 {
 	return _remove_time;
+}
+
+void Attack::SetActive(sf::Vector2i pos, int64 t_point)
+{
+	_attack_obj.ForceMove(pos);
+	_remove_time = t_point;
+
+	_attack_obj.Show();
 }
 
 void Attack::Draw(sf::RenderWindow& window)
@@ -31,7 +39,11 @@ void Attack::Update(const int64 delta_time)
 	_remove_time -= delta_time;
 }
 
-bool Attack::Validate() const
+bool Attack::Validate()
 {
-	return _remove_time > 0;
+	bool validate = _remove_time > 0;
+	if (not validate)
+		_attack_obj.Hide();
+
+	return validate;
 }
--- Client/Attack.h
+++ Client/Attack.h
@@ -5,18 +5,20 @@ class Attack
 {
 private:
 	Object			_attack_obj;
-	int64			_remove_time;
+	int64			_remove_time = 0;
 
 public:
 	Attack();
-	Attack(std::string text, sf::Vector2i pos, int64 t_point);
+	Attack(std::string text);
 
 	bool operator<(const Attack& other) const;
+
+	void SetActive(sf::Vector2i pos, int64 t_point);
 
 	void Draw(sf::RenderWindow& window);
 
 	void Update(const int64 delta_time);
-	bool Validate() const;
+	bool Validate();
 
 };
 
